2b3087e1d5e76f6fbaabe8d0d8282121157eafaa
Andrea Mazzon <mazzon@math.lmu.de>
2020-06-20 15:10:19 +0200

    Package info

---
+++ src/main/java/com/andreamazzon/exercise9/approximationschemes/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * In this package we deal with the simulation of a general stochastic process for a general scheme,
+ * and as examples we see the simulation of a Black-Scholes model for Euler, log Euler, and Milstein
+ * schemes. We then test the methods by computing the price of a call option.
+ */
+package com.andreamazzon.exercise9.approximationschemes;
